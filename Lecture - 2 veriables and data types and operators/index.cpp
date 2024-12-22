19e2df9aad5c724b59d37a2aec6bcd8168cf1d81
Piyush Regar <piyushdadwadiya@gmail.com>
2024-12-22 19:43:03 +0530

    Add files via upload

---
+++ Lecture - 2 veriables and data types and operators/index.cpp
@@ -0,0 +1,43 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     cout << "Piyush\nregar"<< "\n"; 
+
+//     return 0;
+// }
+
+
+//---------------- this is boilerprint code -----------
+
+
+//------------veriables -----------
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//    char grade = 'a';
+
+//    int value = grade;
+//    cout << value << endl;
+// }
+// // int a = 10;
+// // int b = 20;
+
+
+/// ---------------- input -------------
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    cout << "Enter the age: ";
+    cin>>age;
+
+    cout <<"your age is: " << age << endl;
+    return 0;
+}
